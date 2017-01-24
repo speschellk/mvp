f823b6b2eace5d9f0fdef301c9f24350cfa95be1
Kay Christensen <kaychristensen@gmail.com>
2017-01-23 17:18:43 -0800

    Add drinks controller and drinks html

---
+++ client/app/drinks/drinks.js
@@ -0,0 +1,18 @@
+angular.module('cocktail-menu.drinks', [])
+
+.controller('DrinksController', function ($scope, Drinks) {
+
+  $scope.data = {};
+
+  var initializeDrinks = function () {
+    Drinks.getAll()
+      .then(function (drinks) {
+        $scope.data.drinks = drinks;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  initializeDrinks();
+  });
