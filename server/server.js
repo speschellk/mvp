179fc292edbfb6b058f0338e37fca6bbba6f0476
Kay Christensen <kaychristensen@gmail.com>
2017-01-23 16:14:35 -0800

    Server connects

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+
+// connect to database
+mongoose.connect('mongodb://localhost/cocktail-menu');
+
+// configure server with middleware and routes
+// require('./config/middleware.js')(app, express);
+// require('./config/routes.js')(app, express);
+
+// listen on port 3000
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
+
+// export app
+module.exports = app;
