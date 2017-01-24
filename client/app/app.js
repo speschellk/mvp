f5685419b2ad69d7573cad8a450e6d56800ee56d
Kay Christensen <kaychristensen@gmail.com>
2017-01-24 00:58:58 -0800

    Clean up and comment

--- client/app/app.js
+++ client/app/app.js
@@ -3,10 +3,3 @@
 // next line tells angular to create a module called 'cocktail' with no dependencies
 // (omitting the array would tell it to go find the module called 'cocktail')
 angular.module('cocktail', []);
-// .config(function ($routeProvider) {
-//   $routeProvider
-//     .when('/', {
-//       templateUrl: './index.html',
-//       controller: 'AuthController'
-//     });
-// });
