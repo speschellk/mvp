55e5f88241461e396c784ac661b8b7ef616e5567
Kay Christensen <kaychristensen@gmail.com>
2017-01-24 00:59:21 -0800

    Push search results to drinks array

--- client/app/drinks/drinks.js
+++ client/app/drinks/drinks.js
@@ -3,38 +3,21 @@ angular.module('cocktail', [])
 
 // make a controller on it
   .controller('DrinksController', function($scope, $http) {
-    $scope.search = 'Vodka';
-    // console.log('inside controller')
     $scope.$watch('search', function() {
       fetch($scope, $http);
     });
   });
 
 function fetch($scope, $http) {
-  // console.log('calling fetch function')
   $http({
     method: 'GET',
     url: 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + $scope.search
   })
   .then(function(res) {
-    console.log('data is', res.data.drinks[0].strDrink);
-    $scope.drinks = res.data.drinks[0].strDrink;
+    $scope.drinks = res.data.drinks;
 
   })
   .catch(function(err) {
     console.log('Spilled your drink!', err);
   });
 }
-
-  // var initializeDrinks = function () {
-  //   Drinks.getAll()
-  //     .then(function (drinks) {
-  //       $scope.data.drinks = drinks;
-  //     })
-  //     .catch(function (error) {
-  //       console.error(error);
-  //     });
-  // };
-
-  // initializeDrinks();
-// });
