0eacb8df787bbe6150659f10b0363b9d6d7be922
Kay Christensen <kaychristensen@gmail.com>
2017-01-24 12:29:39 -0800

    Filter by two ingredients working

--- client/app/drinks/drinks.js
+++ client/app/drinks/drinks.js
@@ -6,25 +6,67 @@ angular.module('cocktail', [])
     $scope.$watch('search', function() {
       fetchByIngredient($scope, $http);
     });
+    $scope.$watch('search2', function() {
+      filterByIngredient($scope, $http);
+    });
   });
 
 function fetchByIngredient($scope, $http) {
+  var ingredientResults = $scope.drinks;
+  $scope.ingredientResults = ingredientResults;
+
   $http({
     method: 'GET',
     url: 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + $scope.search
   })
   .then(function(res) {
-    // console.log('drinks data is', res.data.drinks);
+    console.log('drinks data is', res.data.drinks);
     for (var i = 0; i < res.data.drinks.length; i++) {
       // console.log('in the for loop');
       if (res.data.drinks[i].strDrinkThumb !== null) {
         // console.log('in the if check');
-        $scope.drinks.push(res.data.drinks[i]);
+        ingredientResults.push(res.data.drinks[i]);
       }
     }
-    return $scope.drinks;
-    // $scope.drinks = res.data.drinks;
+    return ingredientResults;
+  })
+  .catch(function(err) {
+    console.log('Spilled your drink!', err);
+  });
+}
 
+function filterByIngredient($scope, $http, ingredientResults) {
+  $scope.filterResults = [];
+
+  $http({
+    method: 'GET',
+    url: 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + $scope.search2
+  })
+  .then(function(res) {
+    console.log('second drinks data is', res.data.drinks);
+    for (var i = 0; i < res.data.drinks.length; i++) {
+      console.log('in the filter for loop');
+      // console.log('in the for loop');
+      if (res.data.drinks[i].strDrinkThumb !== null) {
+        console.log('in the filter if check');
+        // console.log('in the if check');
+        var id = res.data.drinks[i].idDrink;
+
+        for (var j = 0; j < $scope.ingredientResults.length; j++) {
+          console.log('ingredient results id', $scope.ingredientResults[j].idDrink);
+          if ($scope.ingredientResults[j].idDrink === id) {
+            $scope.filterResults.push(res.data.drinks[i]);
+          }
+        }
+        // if ($scope.ingredientResults.includes(res.data.drinks[i])) {
+        //   console.log('checking if first results include filter results');
+        //   $scope.filterResults.push(res.data.drinks[i]);
+        // }
+      }
+    }
+    $scope.drinks = $scope.filterResults;
+    console.log('filtered results are', $scope.filterResults);
+    return $scope.drinks;
   })
   .catch(function(err) {
     console.log('Spilled your drink!', err);
