77f9137f37be78a3e4d57c10f54bba6d1d52e48e
Kay Christensen <kaychristensen@gmail.com>
2017-01-24 11:59:26 -0800

    Display only results that have a thumbnail

--- client/app/drinks/drinks.js
+++ client/app/drinks/drinks.js
@@ -4,18 +4,26 @@ angular.module('cocktail', [])
 // make a controller on it
   .controller('DrinksController', function($scope, $http) {
     $scope.$watch('search', function() {
-      fetch($scope, $http);
+      fetchByIngredient($scope, $http);
     });
   });
 
-function fetch($scope, $http) {
+function fetchByIngredient($scope, $http) {
   $http({
     method: 'GET',
     url: 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + $scope.search
   })
   .then(function(res) {
-    console.log(res.data.drinks);
-    $scope.drinks = res.data.drinks;
+    // console.log('drinks data is', res.data.drinks);
+    for (var i = 0; i < res.data.drinks.length; i++) {
+      // console.log('in the for loop');
+      if (res.data.drinks[i].strDrinkThumb !== null) {
+        // console.log('in the if check');
+        $scope.drinks.push(res.data.drinks[i]);
+      }
+    }
+    return $scope.drinks;
+    // $scope.drinks = res.data.drinks;
 
   })
   .catch(function(err) {
