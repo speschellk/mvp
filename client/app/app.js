eaf667c40c36b8f0f003cd1fad2008b20b0d24b3
Kay Christensen <kaychristensen@gmail.com>
2017-01-24 23:15:50 -0800

    Clean up comments

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,1 @@
-// app.js file defines our app, and that's it.
-
-// next line tells angular to create a module called 'cocktail' with no dependencies
-// (omitting the array would tell it to go find the module called 'cocktail')
 angular.module('cocktail', []);
